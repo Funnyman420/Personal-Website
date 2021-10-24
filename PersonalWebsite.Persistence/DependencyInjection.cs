596e6dad9b22b34ab95a018fe3548c14a5f7a54f
mikeg <mikegeor@outlook.com.gr>
2021-10-24 18:38:16 +0300

    Connected MySQL Server
    Completed the connection between MySQL Server and EntityFrameworkCore

---
+++ PersonalWebsite.Persistence/DependencyInjection.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+
+
+namespace PersonalWebsite.Persistence
+{
+    public static class DependencyInjection
+    {
+        public static IServiceCollection AddPersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            var connectionString = configuration.GetConnectionString("BlogDatabase");
+
+            services.AddDbContext<PersonalWebsiteDbContext>(options =>
+            {
+                options.UseMySQL(connectionString);
+            });
+
+
+            return services;
+        }
+    }
+}
---
+++ PersonalWebsite.Persistence/PersonalWebsiteDbContext.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PersonalWebsite.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace PersonalWebsite.Persistence
+{
+    class PersonalWebsiteDbContext : DbContext
+    {
+        public PersonalWebsiteDbContext(DbContextOptions<PersonalWebsiteDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Blogger> Bloggers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+            builder.ApplyConfigurationsFromAssembly(Assembly.GetExecutingAssembly());
+        }
+    }
+}
