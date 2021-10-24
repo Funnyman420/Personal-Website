c4798286c05386a1ffefeebfa468f52bc0bced77
mikeg <mikegeor@outlook.com.gr>
2021-10-24 18:43:06 +0300

    Finished Project Structure
    These are empty projects that represent the architecture of the application

---
+++ PersonalWebsite.Domain/Entities/Blogger.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonalWebsite.Domain.Entities
+{
+    public class Blogger
+    {
+        public string BloggerId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Title { get; set; }
+        public string Description { get; set; }
+    }
+}
